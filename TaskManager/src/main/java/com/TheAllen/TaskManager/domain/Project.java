eff2bbf5098a7313106b5e2696487223434d7401
Allen Li <allenlee1995@hotmail.com>
2019-06-19 22:14:41 -0600

    Show Hello World screen

--- TaskManager/src/main/java/com/TheAllen/TaskManager/domain/Project.java
+++ TaskManager/src/main/java/com/TheAllen/TaskManager/domain/Project.java
@@ -1,5 +1,14 @@
 package com.TheAllen.TaskManager.domain;
 
-public class Project {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class Project {
+	
+	@RequestMapping(path="/helloworld", method=RequestMethod.GET)
+	public String helloWorld() {
+		return "Hello World";
+	}
 }
