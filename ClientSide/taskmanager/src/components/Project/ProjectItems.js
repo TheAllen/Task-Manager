c2e40f79cbafb8f23dfe754de886fc29f4fa8d5c
Allen Li <allenlee1995@hotmail.com>
2019-06-25 20:52:56 -0600

    styled main page

---
+++ ClientSide/taskmanager/src/components/Project/ProjectItems.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ProjectItems extends Component {
+    render() {
+        return(
+            <h3>Project Items</h3>
+        );
+    }
+}
