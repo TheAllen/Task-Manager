090b2ada81373d1b20dea22c77a81d9fd8de9b27
Allen Li <allenlee1995@hotmail.com>
2019-07-03 22:02:42 -0600

    redux get all projects

--- ClientSide/taskmanager/src/components/dashboard.js
+++ ClientSide/taskmanager/src/components/dashboard.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 import ProjectItem from './Project/ProjectItems'
 import Header from './Layout/Header'
 import CreateTaskButton from './Project/CreateTaskButton'
+import { connect } from 'react-redux'
+import { getProjects} from '../actions/projectAction'
+import PropTypes from "prop-types"
 
 class DashBoard extends Component {
+
+    componentDidMount(){
+        this.props.getProjects();
+    }
+
     render() {
         return (
             <div>
@@ -12,9 +20,9 @@ class DashBoard extends Component {
                         <div className="row">
                             <div className="col-md-12">
                                 <h1 className="display-4 text-center">Tasks and Todos</h1>
-                                    <br />
-                                        <CreateTaskButton/>
-                                    <br />
+                                <br />
+                                <CreateTaskButton />
+                                <br />
                                 <hr />
                                 <ProjectItem />
                             </div>
@@ -30,4 +38,13 @@ class DashBoard extends Component {
     }
 }
 
-export default DashBoard;
+DashBoard.propTypes = {
+    projects: PropTypes.object.isRequired,
+    getProjects:PropTypes.func.isRequired
+}
+
+const mapStateToProps = state => ({
+    project:state.project,
+});
+
+export default connect(mapStateToProps, {getProjects})(DashBoard);
