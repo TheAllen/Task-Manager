b5386b826079444daa9649e0499df6bc6dc41d7b
Allen Li <allenlee1995@hotmail.com>
2019-07-20 18:47:41 -0600

    set up spring security

--- TaskManager/src/main/java/com/TheAllen/TaskManager/security/SecurityConfig.java
+++ TaskManager/src/main/java/com/TheAllen/TaskManager/security/SecurityConfig.java
@@ -1,8 +1,43 @@
 package com.TheAllen.TaskManager.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint entryPoint;
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(entryPoint).and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.headers().frameOptions().sameOrigin() //For H2 database
+		.and()
+		.authorizeRequests()
+		.antMatchers(
+				"/",
+                "/favicon.ico",
+                "/**/*.png",
+                "/**/*.gif",
+                "/**/*.svg",
+                "/**/*.jpg",
+                "/**/*.html",
+                "/**/*.css",
+                "/**/*.js"
+		).permitAll()
+		.anyRequest().authenticated();
+	}
+
+	
 }
