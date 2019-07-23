8402ac9d57c6814e6483a7e5fbabd6c2e7c371d5
Allen Li <allenlee1995@hotmail.com>
2019-07-22 22:25:10 -0600

    JWT provider

---
+++ TaskManager/src/main/java/com/TheAllen/TaskManager/payload/LoginRequest.java
@@ -0,0 +1,33 @@
+package com.TheAllen.TaskManager.payload;
+
+import javax.validation.constraints.NotBlank;
+
+public class LoginRequest {
+	
+	@NotBlank(message="username cannot be blank")
+	private String username;
+	@NotBlank(message="password cannot be blank")
+	private String password;
+	
+
+	public LoginRequest(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
--- TaskManager/src/main/java/com/TheAllen/TaskManager/security/SecurityConfig.java
+++ TaskManager/src/main/java/com/TheAllen/TaskManager/security/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.TheAllen.TaskManager.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.TheAllen.TaskManager.services.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -15,6 +22,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private JwtAuthenticationEntryPoint entryPoint;
+	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+
+	@Override
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -36,7 +64,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/**/*.css",
                 "/**/*.js"
 		).permitAll()
-		.antMatchers("/api/users/**").permitAll()
+		.antMatchers(SecurityConstants.SIGN_UP_URL).permitAll()
+		.antMatchers(SecurityConstants.H2_URL).permitAll()
 		.anyRequest().authenticated();
 	}
 
