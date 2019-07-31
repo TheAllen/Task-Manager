df553a9922ed6697bed06fa73eab2610393a63c7
Allen Li <allenlee1995@hotmail.com>
2019-07-30 19:13:14 -0600

    redirect to dashboard if logged in

--- ClientSide/taskmanager/src/components/UserManagement/Login.js
+++ ClientSide/taskmanager/src/components/UserManagement/Login.js
@@ -19,6 +19,12 @@ class Login extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentDidMount() {
+        if(this.props.security.validToken) {
+            this.props.history.push("/dashboard");
+        }
+    }
+
     onChange(e) {
         this.setState({
             [e.target.name]:e.target.value
